c40b332e634d27cdb663675a125c05562339b12c
Egor <124181347+Reco0n@users.noreply.github.com>
2026-09-07 12:43:07 +0300

    input

--- Danilov_Laba0.cpp
+++ Danilov_Laba0.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout <<"Hello World!\n";
+    cout <<"Hello World!\n";
+    int x;
+    cout<< "Input x: ";
+    cin>>x;
+    if (x !=0)
+        cout<<100/x;
+    return 0;
 }
